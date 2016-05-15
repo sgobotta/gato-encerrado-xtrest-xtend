bd360ba736e69a0a13c197d9488f7b41afa50be2
jesus <laimejesu@gmail.com>
2016-05-15 15:36:26 -0300

    arreglando js

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -5,26 +5,30 @@ loginApp.controller('LoginCtrl', function($scope, $http, $window){
 
 	$scope.username = $scope.username || "";
 	$scope.password = $scope.password || "";
-	
-	$scope.urlToPost = 'localhost:9001/login'
+	$scope.user = {};
+	$scope.urlToPost = 'localhost:9001/login';
 	
 //	$scope.user = {} || "";
     $scope.submitLoginForm = function() {
-    var jsonform = {
+    var formpost = {
     		method  : 'POST',
-            url     : '/login'
-            data    : {$scope.username, $scope.password}//$scope.user,
+            url     : '/login',
+            data    : {},//$scope.user,
             headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-    }
+    };
+    var formjson = JSON.stringify(user);
     //ver como submitear forms
-    $scope.submitForm = function() {
-        console.log("posting data....");
-        $http.post('/login', jsonform)).success(function(){
-        	$window.alert('voy a logear')}), 
-        function errorCallback(data){
-	  		$window.alert('No voy a logear')});
-        }
-});
+    console.log("posting data....");
+    $http.post('/login', form, {headers: {'Content-Type': 'application/json'} })
+    	.success(function(data, status, headers, config){
+        	$window.alert('me logee');
+        	console.log(data);
+        })
+        .error(function (data, status, headers, config){
+        	$window.alert('No pude registrarme');
+        });
+};
+
 
 loginApp.controller('SignUpCtrl', function($scope, $http){
 
@@ -32,24 +36,28 @@ loginApp.controller('SignUpCtrl', function($scope, $http){
 	$scope.username = $scope.username;
 	$scope.password = $scope.password;
 	$scope.repeatpassword = $scope.repeatpassword;
-
+	$scope.user = {};
+	$scope.urlToPost = 'localhost:9001/login';
+	
     $scope.submitSignUpForm = function() {
-    // Posting data to php file
-    $http({
-      method  : 'POST',
-      url     : '/signup',
-      data    : {$scope.username, $scope.password, $scope.repeatpassword},
-      headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-     })
-      .success(function(data) {
-        if (data.errors) {
-          // Showing errors.
-          $scope.errorUserName = data.errors.username;
-          $scope.errorPassword = data.errors.password;
-          $scope.errorRepeatPassword = data.errors.repeatpassword;
-        } else {
-          $scope.message = data.message;
-        }
-      });
+    
+    var formpost= {
+    	      method  : 'POST',
+    	      url     : '/signup',
+    	      data    : $scope.user,
+    	      headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
     };
-})
+    var formjson = JSON.stringify($scope.user);
+    
+        console.log("posting data....");
+        $http.post('/signup', formjson, {headers: {'Content-Type': 'application/json'} })
+        	.success(function(data, status, headers, config){
+        		$window.alert('me registre');
+                console.log(data);
+        })
+        	.error(function (data, status, headers, config){
+        		$window.alert('No pude registrarme');
+	  		});
+        };
+    });
+});
