58b06d9a5ab19952dd0955299c9acd0581d166b0
jesus <laimejesu@gmail.com>
2016-05-20 12:18:39 -0300

    agregando avisos de error que no funcionan

--- src/main/webapp/js/loginApp.js
+++ src/main/webapp/js/loginApp.js
@@ -8,23 +8,27 @@ loginApp.controller('LoginController', function($scope, LoginService, $window) {
 	$scope.login.username = "";
 	$scope.login.password = "";
 	$scope.loginFailure = false;
-	$scope.spanLog = "";
-
+	$scope.loginSpanLog = "";
+	
 	$scope.ingresar = function() {
 		LoginService.login($scope.login, $scope.callback, $scope.errorHandler);
 	};
 	$scope.callback = function(data) {
-		var nuevoUser = data.id;
-		alert("funciono call back");
+		alert("logeando");
 		//hay que cambiarlo pero ya funcioan el post
         var url = "http://" + $window.location.host;
         $window.location.href = url;
 		
 	};
 	$scope.errorHandler = function(error) {
-		$scope.spanLog = error.descripcion;
+		$scope.loginSpanLog = error.descripcion;
 		$scope.loginFailure = true;
 	};
+	$scope.showError = function(){
+	    $timeout(3000);
+		$scope.loginFailure = false;
+		alert($scope.loginSpanLog)
+	}
 });
 loginApp.controller('SignUpController', function($scope, LoginService) {
 
@@ -33,39 +37,46 @@ loginApp.controller('SignUpController', function($scope, LoginService) {
 	$scope.signup.password = "";
 	$scope.signup.repeatpassword = "";
 
+	$scope.signUpFailure = false;
+	$scope.signUpSpanLog = "";	
+	
 	$scope.registrar = function() {
 		LoginService
 				.signup($scope.signup, $scope.callback, $scope.errorHandler);
 	};
 	$scope.callback = function(data) {
-		var nuevoUser = data.id;
-		alert("funciono call back");
+		alert("cuenta creada");
 	};
 	$scope.errorHandler = function(error) {
-		$scope.spanLog = error.descripcion;
-		$scope.loginFailure = true;
+		$scope.signUpSpanLog = error.descripcion;
+		$scope.signUpFailure = true;
 	};
+	$scope.showError = function(){
+	    $timeout(3000);
+		$scope.signUpFailure = false;
+		alert($scope.signUpSpanLog)
+	}
 });
 
 loginApp.service("LoginService", function($http) {
-	this.login = function(jsonObject, callback, errorHandler) {
+	
+	this.httpFunction = function(url, method, user, callback, errorHandler){
 		$http({
-			url : 'http://localhost:9001/login',
-			method : "POST",
-			data : jsonObject,
+			url : url,
+			method : method,
+			data : user,
 			headers : {
 				'Content-Type' : 'application/x-www-form-urlencoded'
 			}
 		}).success(callback).error(errorHandler);
+		
 	};
-	this.signup = function(jsonObject, callback, errorHandler) {
-		$http({
-			url : 'http://localhost:9001/signup',
-			method : "POST",
-			data : jsonObject,
-			headers : {
-				'Content-Type' : 'application/x-www-form-urlencoded'
-			}
-		}).success(callback).error(errorHandler);
+	
+	this.login = function(user, callback, errorHandler){
+		this.httpFunction('http://localhost:9001/login', "POST", user, callback, errorHandler);		
+	};
+
+	this.signup = function(user, callback, errorHandler) {
+		this.httpFunction('http://localhost:9001/signup', "POST", user, callback, errorHandler);
 	};
 });
